0d54c2c2b778436e238b2fbf019943d32eaec51a
e_konstantinidis <e.konstantinidis@app-smart.de>
2017-05-05 11:21:23 +0200

    Finished Nested Resources: Part 2

--- i_reviewed/db/seeds.rb
+++ i_reviewed/db/seeds.rb
@@ -6,3 +6,9 @@ Book.create! [
     { name: "Metaprogramming Ruby 2", author: "Paolo Perrotta"},
     { name: "Design Patterns in Ruby", author: "Russ Olsen"},
     { name: "The Ruby Programming Language", author: "David Flanagan"}]
+
+eloquent = Book.find_by name: "Eloquent Ruby"
+eloquent.notes.create! [
+    { title: "Wow", note: "Great book to learn Ruby"},
+    { title: "Funny", note: "Doesn't put you to sleep"}
+                       ]
